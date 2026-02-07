0e927c486a5db4cecd549ffe3c11e81bc969067e
Joemarroque <roquejoemar5@gmail.com>
2026-02-07 09:21:13 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBZZd3uQ3oXhupTLdZ8Ou03spMrVZh2iWk",
+  authDomain: "bawud-restobar-survey-form.firebaseapp.com",
+  projectId: "bawud-restobar-survey-form",
+  storageBucket: "bawud-restobar-survey-form.firebasestorage.app",
+  messagingSenderId: "627201409239",
+  appId: "1:627201409239:web:68330125fbf216959db092",
+  measurementId: "G-7X9K54P0TH"
+};
